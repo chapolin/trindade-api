88ca8cf8ea0af6d5acdacb479893430d0f527116
dtrinda <danilo.trindade@walmart.com>
2017-06-29 23:52:29 -0300

    Add oitavas in object model

--- models/Cartola.js
+++ models/Cartola.js
@@ -1,10 +1,12 @@
 (function() {
   "use strict";
 
-  let Cartola = exports.Cartola = function(key, grupos, confrontosFase1Ida, confrontosFase1Volta) {
+  let Cartola = exports.Cartola = function(key, grupos, confrontosFase1Ida, confrontosFase1Volta, confrontosOitavasIda, confrontosOitavasVolta) {
     this.key = key;
     this.grupos = grupos;
     this.confrontosFase1Ida = confrontosFase1Ida;
     this.confrontosFase1Volta = confrontosFase1Volta;
+    this.confrontosOitavasIda = confrontosOitavasIda;
+    this.confrontosOitavasVolta = confrontosOitavasVolta;
   };
 })();
